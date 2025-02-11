8e6659b7be373ef721836f7e5094de24067b9543
Hesham <Hesham Abdelnasser@DESKTOP-D15PJJA>
2025-02-11 22:43:44 +0200

    fixed media +1

--- src/components/Slider/slider.jsx
+++ src/components/Slider/slider.jsx
@@ -1,7 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './slider.css';
+import './slider.css'
 const images = [
   "/نوم.jpg",
   "/خزانات ملابس.jpg",
@@ -28,10 +28,10 @@ function ImageSlider() {
   };
 
   return (
-    <Slider className="divSlider" {...settings} style={{ margin: "auto", borderRadius: "50px" ,}}>
+    <Slider className="divSlider" {...settings} style={{ width: "500px", margin: "auto", borderRadius: "50px" ,}}>
       {images.map((src, index) => (
-        <div  key={index}>
-          <img className="imgSlider" src={src} alt={`Slide ${index}`}  />
+        <div key={index}>
+          <img className="imgSlider" src={src} alt={`Slide ${index}`} width="100%" height="350px" />
         </div>
       ))}
     </Slider>
